e29f70e9007e1ad143562567538259b0e7e48a47
LINGAMURTHY M P <lingamurthy5364@gmail.com>
2022-06-26 22:33:02 +0530

    first push for tshirtstore project

---
+++ app.js
@@ -0,0 +1,52 @@
+const express= require('express')
+require('dotenv').config()
+const app= express()
+const morgan= require('morgan')
+const cookieParser= require('cookie-parser')
+const fileUpload= require('express-fileupload')
+
+
+//regular middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+//cookie and file middleware
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/tmp/"
+}))
+
+
+
+//temp check
+app.set("view engine", "ejs")
+//morgan middleware
+app.use(morgan('tiny'))
+//import all routes here    
+const home = require('./routes/home')
+const user = require('./routes/user')
+const product = require('./routes/product')
+const order = require('./routes/order')
+
+// for swagger documentation
+const swaggerUi= require("swagger-ui-express")
+const YAML= require("yamljs")
+const swaggerDocument= YAML.load("./swagger.yaml")
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+//router middleware
+app.use('/api/v1', home)
+app.use('/api/v1', user)
+app.use('/api/v1', product)
+app.use('/api/v1', order)
+
+
+
+app.get('/signuptest', (req, res)=>{
+    res.render('signuptest')
+})
+//export app js 
+module.exports= app
---
+++ utils/emailHelper.js
@@ -0,0 +1,27 @@
+const nodemailer= require("nodemailer")
+
+const mailHelper= async(option)=>{
+
+    let transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+       
+        auth: {
+          user: process.env.SMTP_USER, // generated ethereal user
+          pass: process.env.SMTP_PASS, // generated ethereal password
+        },
+      });
+    const message={
+        
+            from: 'lingamurthy5364@gmail.com', // sender address
+            to: option.email, // list of receivers
+            subject: option.subject, // Subject line
+            text: option.message, // plain text body
+            //html: "<b>Hello world?</b>", // html body
+          }
+    
+      // send mail with defined transport object
+      await transporter.sendMail(message);
+}
+
+module.exports= mailHelper
